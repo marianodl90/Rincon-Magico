afdb122d1514ebb3f4c9dfc820e4b35ea66586a4
Mariano de luis <marianodeluis1990@gmail.com>
2025-06-13 18:27:26 -0300

    me olvide el models de la app de mensajeria, ya se puede ver en la base de datos

--- mensajeria/admin.py
+++ mensajeria/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Mensaje
+
+@admin.register(Mensaje)
+class MensajeAdmin(admin.ModelAdmin):
+    list_display = ('remitente', 'destinatario', 'contenido', 'fecha_envio')
+    search_fields = ('remitente__username', 'destinatario__username', 'contenido')
